9a586bc7acd401c50c876920db29e1e365dbc315
JJRPF <jjrpfister@gmail.com>
2022-12-05 17:14:21 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+myfamily = ["mom","dad","justin","madison","raegan","mimi","boppy"]
+for i in (myfamily):
+    print("i love " + i)
+t=8004
+m=99
+b=10
+x=21
+y=m*x+b*t
+y = str(y)
+
+print("the answer is " + y )
+
+
