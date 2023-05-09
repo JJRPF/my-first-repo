4516e79d8322711c978cdbc746bda6868b94ba31
JJRPF <jjrpfister@gmail.com>
2023-05-09 08:10:57 -0400

    test

--- AI-game.py
+++ AI-game.py
@@ -1,34 +1,191 @@
-import pygame
-import sys
-import random
-import time
+import tkinter as tk
 
-pygame.init()
+# Set the window size
+window_size = (400, 400)
 
-#colors
-white = (255,255,255)
-black = (0,0,0)
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
+# Create a window
+window = tk.Tk()
 
-#display
-display_width = 800
-display_height = 600
-gameDisplay = pygame.display.set_mode((display_width,display_height))
-pygame.display.set_caption('Cookie Clicker')
-clock = pygame.time.Clock()
+# Set the window title
+window.title("Virtual Speedcube")
 
-#images
-cookieImg = pygame.image.load('cookie.png')
-cookieImg = pygame.transform.scale(cookieImg, (50,50))
+# Set the window size
+window.geometry("{}x{}".format(window_size[0], window_size[1]))
 
-#cookie clicker
-cookieClicker = 0
-cookieClickerPerSec = 1
-cookieClickerPerSecCost = 10
-cookieClickerPerSecCost2 = 50
-cookieClickerPerSecCost3 = 100
-cookieClickerPerSecCost4 = 150
-cookieClickerPerSecCost5 = 200
-cookieClickerPerSecCost
+# Set the block size
+block_size = 20
+
+# Set the colors for the different block faces
+colors = {
+    "U": "white",  # up
+    "D": "yellow",  # down
+    "F": "green",  # front
+    "B": "blue",  # back
+    "L": "orange",  # left
+    "R": "red",  # right
+}
+
+# Create a list to represent the cube
+cube = [
+    ["U", "U", "U", "U", "U", "U", "U", "U", "U"],  # up face
+    ["L", "L", "L", "L", "L", "L", "L", "L", "L"],  # left face
+    ["F", "F", "F", "F", "F", "F", "F", "F", "F"],  # front face
+    ["R", "R", "R", "R", "R", "R", "R", "R", "R"],  # right face
+    ["B", "B", "B", "B", "B", "B", "B", "B", "B"],  # back face
+    ["D", "D", "D", "D", "D", "D", "D", "D", "D"],  # down face
+]
+
+# Create a canvas to draw the cube on
+canvas = tk.Canvas(window, width=window_size[0], height=window_size[1])
+canvas.pack()
+
+# Create a function to draw the cube
+def draw_cube():
+  # Clear the canvas
+  canvas.delete("all")
+
+  # Calculate the center of the window
+  center_x = window_size[0] / 2
+  center_y = window_size[1] / 2
+
+  # Calculate the start position for the front face
+  start_x = center_x - (block_size * 1.5)
+  start_y = center_y - (block_size * 1.5)
+  # Draw the front face
+  for i in range(3):
+    for j in range(3):
+      color = colors[cube[2][i * 3 + j]]
+      x = start_x + (j * block_size)
+      y = start_y + (i * block_size)
+      canvas.create_rectangle(x, y, x + block_size, y + block_size, fill=color)
+
+  # Calculate the start position for the right face
+  start_x += block_size * 3
+  start_y -= block_size
+
+  # Draw the right face
+  for i in range(3):
+    for j in range(3):
+      color = colors[cube[3][i * 3 + j]]
+      x = start_x + (j * block_size)
+      y = start_y + (i * block_size)
+      canvas.create_rectangle(x, y, x + block_size, y + block_size, fill=color)
+
+  # Calculate the start position for the back face
+  start_x += block_size
+  start_y -= block_size * 3
+
+  # Draw the back face
+  for i in range(3):
+    for j in range(3):
+      color = colors[cube[4][i * 3 + j]]
+      x = start_x + (j * block_size)
+      y = start_y + (i * block_size)
+      canvas.create_rectangle(x, y, x + block_size, y + block_size, fill=color)
+
+  # Calculate the start position for the left face
+  start_x -= block_size * 4
+  start_y -= block_size
+
+  # Draw the left face
+  for i in range(3):
+    for j in range(3):
+      color = colors[cube[1][i * 3 + j]]
+      x = start_x + (j * block_size)
+      y = start_y + (i * block_size)
+      canvas.create_rectangle(x, y, x + block_size, y + block_size, fill=color)
+
+  # Calculate the start position for the top face
+  start_x += block_size * 2
+  start_y -= block_size * 2
+
+  # Draw the top face
+  for i in range(3):
+    for j in range(3):
+      color = colors[cube[0][i * 3 + j]]
+      x = start_x + (j * block_size)
+      y = start_y + (i * block_size)
+      canvas.create_rectangle(x, y, x + block_size, y + block_size, fill=color)
+
+  # Calculate the start position for the bottom face
+  start_x -= block_size
+  start_y -= block_size
+
+  # Draw the bottom face
+  for i in range(3):
+    for j in range(3):
+      color = colors[cube[5][i * 3 + j]]
+      x = start_x + (j * block_size)
+      y = start_y + (i * block_size)
+      canvas.create_rectangle(x,)
+      canvas.create_rectangle(x, y, x + block_size, y + block_size, fill=color)
+
+# Call the draw_cube function to draw the initial state of the cube
+draw_cube()
+
+# Create a function to handle user input
+def handle_input(event):
+  # Get the key that was pressed
+  key = event.char
+
+  # Rotate the front face clockwise
+  if key == "w":
+    # Get the front face
+    front = cube[2]
+
+    # Rotate the front face
+    front = [front[6], front[3], front[0], front[7], front[4], front[1], front[8], front[5], front[2]]
+
+    # Update the cube with the rotated face
+    cube[2] = front
+
+    # Redraw the cube
+    draw_cube()
+
+  # Rotate the front face counterclockwise
+  elif key == "s":
+    # Get the front face
+    front = cube[2]
+
+    # Rotate the front face
+    front = [front[2], front[5], front[8], front[1], front[4], front[7], front[0], front[3], front[6]]
+
+    # Update the cube with the rotated face
+    cube[2] = front
+
+    # Redraw the cube
+    draw_cube()
+
+  # Rotate the right face clockwise
+  elif key == "d":
+    # Get the right face
+    right = cube[3]
+
+    # Rotate the right face
+    right = [right[6], right[3], right[0], right[7], right[4], right[1], right[8], right[5], right[2]]
+
+    # Update the cube with the rotated face
+    cube[3] = right
+
+    # Redraw the cube
+    draw_cube()
+
+  # Rotate the right face counterclockwise
+  elif key == "a":
+    # Get the right face
+    right = cube[3]
+
+    # Rotate the right face
+    right = [right[2], right[5], right[8], right[1], right[4], right[7], right[0], right[3], right[6]]
+
+    # Update the cube with the rotated face
+    cube[3] = right
+
+    # Redraw the cube
+    draw_cube()
+
+# Bind the handle_input function to the "key" event
+window.bind("<Key>", handle_input)
+
+# Run the main loop
+window.mainloop()
